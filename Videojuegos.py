d0555c55b68012856f10fa7837b34f98c8e7fbf1
Thomra21 <133516173+Thomra21@users.noreply.github.com>
2023-05-13 18:34:29 -0500

    Add files via upload

---
+++ Videojuegos.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+import sys
+pygame.init()
+import os
+import subprocess
+
+# Crear display window
+altura = 700
+ancho = 1200
+
+screen = pygame.display.set_mode((ancho, altura))
+pygame.display.set_caption('Videojuegos')
+fuente=pygame.font.Font(None,50)
+text="Videojuegos para tu entretenimiento :)"
+mensaje=fuente.render(text,1,(255,255,255))
+
+mensaje_rect = mensaje.get_rect()
+mensaje_rect.centerx = screen.get_rect().centerx
+
+star_img = pygame.image.load('Pacman.png').convert_alpha()
+star_img2 = pygame.image.load('serpiente.png').convert_alpha()
+star_img3 = pygame.image.load('dinosaurio.png').convert_alpha()
+
+class button():
+    def __init__(self,x,y, image):
+        self.image= image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
+    def dibujo(self):
+        action= False
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        
+
+        #check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action= True
+                
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+
+        screen.blit(self.image,(self.rect.x, self.rect.y))
+        return action
+
+
+star_button = button(100,200, star_img)
+star_button2 = button(450,200, star_img2)
+star_button3 = button(800,200, star_img3)
+
+
+#game loop
+run = True
+while run:
+    screen.fill((0,0,0))
+    #screen.blit(mensaje,(15,10))
+    screen.blit(mensaje, mensaje_rect)
+
+    if star_button.dibujo():
+        subprocess.run(['python','pacman.py'])
+    if star_button2.dibujo():
+        subprocess.run(['python','snake.py'])
+    if star_button3.dibujo():
+        subprocess.run(['python','dinosaurio.py'])
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+
+    pygame.display.update()
+ 
+    
+
+pygame.quit()
